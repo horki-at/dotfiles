d0dc8ff3c17e904578b29bc0ab35e7dffe04df32
horki-at <arthurtopal342@gmail.com>
2025-12-26 18:51:49 +0100

    created tasks plugin

---
+++ .config/emacs/sources/tasks.el
@@ -0,0 +1,18 @@
+;; Tasks Runner (similar to Visual Studio Code tasks.json)
+
+(defvar-local tasks/alist nil
+  "An alist of (TaskName . Cmd) for the current project.")
+
+(defun tasks/run-task ()
+  "Prompt for a task defined in the local tasks/alist and execute it."
+  (interactive)
+  (if (not tasks/alist)
+      (user-error "No tasks defined. Create it in .dir-locals.el")
+    (let* ((choices (mapcar #'car tasks/alist))
+           (selected (completing-read "Run task: " choices))
+           (cmd (alist-get selected choices nil nil #'string=)))
+      (cond
+       ((stringp cmd) (compile cmd))    ; string => run shell command
+       ((functionp cmd) (funcall cmd))  ; elisp func => execute it within emacs
+       (t (user-error "Invalid task format: %s" cmd))))))
+(global-set-key (kbd "C-c t") #'tasks/run-task)
