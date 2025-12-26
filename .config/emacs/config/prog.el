3f5034d22cd81c1120320a11a35d88e34db615ae
horki-at <arthurtopal342@gmail.com>
2025-12-26 13:27:08 +0100

    update C/C++ styles

--- .config/emacs/config/prog.el
+++ .config/emacs/config/prog.el
@@ -1,9 +1,12 @@
 (defun my-c-style ()
   (c-set-style "bsd")
+  (c-set-offset 'member-init-intro 0)
   (setq c-basic-offset 2)
   (setq indent-tabs-mode nil))
 
+(add-hook 'c-mode-hook 'my-c-style)
 (add-hook 'c++-mode-hook 'my-c-style)
+
 (add-to-list 'auto-mode-alist '("\\.ih\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.inl\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.tpp\\'" . c++-mode))
