6df83f2d2c0e6c3948824c92cdc26ed0f253900f
horki-at <arthurtopal342@gmail.com>
2026-01-03 17:01:47 +0100

    Reorganized elisp sources, and ensured TS major mode for C/C++ srcs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,12 +1,9 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
-(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "src" user-emacs-directory))
 (load-library "core")
-(load-library "prog")
 (load-library "vendor")
-
-(add-to-list 'load-path (expand-file-name "sources" user-emacs-directory))
 (load-library "tasks")
 (load-library "sticky-note")
 (load-library "sidebar")
--- .config/emacs/src/core.el
+++ .config/emacs/src/core.el
@@ -1,4 +1,4 @@
-; Performance
+;; Performance
 (setq gc-cons-threshold 100000000)
 (setq read-process-output-max (* 1024 1024)) ; 1mb
 
@@ -7,6 +7,8 @@
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
 
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+(which-function-mode 1)
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
 (setq ring-bell-function 'ignore)
@@ -20,6 +22,23 @@
 (menu-bar-mode 0)
 (global-display-fill-column-indicator-mode)
 (global-hl-line-mode)
+
+(setq c-basic-offset 2)
+(setq c-default-style "bsd")
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.c\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.cc\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.hh\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.ih\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.inl\\'" . c-or-c++-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.tpp\\'" . c-or-c++-ts-mode))
+
+(setq treesit-language-source-alist
+      '((cpp "https://github.com/tree-sitter/tree-sitter-cpp" "v0.22.3")
+        (c "https://github.com/tree-sitter/tree-sitter-c" "v0.23.6")))
+
+(keymap-global-set "C-c r" 'recompile)
 
 (custom-set-faces
  '(dired-directory ((t (:weight bold))))
